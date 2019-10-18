cd814f717ab2b46fe0f9c71c0d64908c0180c31d
Oleksandr Andrushchenko <andr2000@gmail.com>
2019-10-18 22:34:25 +0300

    Add possibility to send arbitrary commands to ebusd
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- ebusdpy/__init__.py
+++ ebusdpy/__init__.py
@@ -1,4 +1,4 @@
 
 """A set of wrapper functions for accessing the eBusd API."""
 
-from .ebusdpy import (init, read, write)
+from .ebusdpy import (init, read, write, raw)
--- ebusdpy/ebusdpy.py
+++ ebusdpy/ebusdpy.py
@@ -58,6 +58,27 @@ def write(address, circuit, name, value):
         sock.close()
     return result
 
+
+def raw(address, command):
+    result = None
+    try:
+        """ Open the socket at the specified address, call the command sent and return data """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect(address)
+        """ Send the command """
+        sock.sendall((command + '\n').encode())
+        """ Get the result decoded UTF-8 """
+        result = sock.recv(256).decode('utf-8').rstrip()
+    except socket.timeout:
+        raise RuntimeError(socket.timeout)
+    except socket.error:
+        raise RuntimeError(socket.error)
+    finally:
+        sock.close()
+    return result
+
+
 def humanize(type, value):
     _state = None
     if type == 0:
